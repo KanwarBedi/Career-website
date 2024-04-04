265c6100908325a4ae958a09b5413c9f477b83aa
KanwarBedi <bedipreetkanwar@gmail.com>
2024-04-04 13:05:30 +0530

    tried dynamic

--- app.py
+++ app.py
@@ -2,9 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+jobs = [
+            {'id' : 1,
+                'title': 'Data Scientist',
+                'Location': 'Bengaluru',
+                'salary': 'Rs 12 lpa'
+            },
+                {'id' : 2,
+                    'title': 'Data Analyst',
+                    'Location': 'Bengaluru',
+                    'salary': 'Rs 10 lpa'
+                }
+        ]
+
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html',jobs = jobs)
 
 
 if __name__ == "__main__":
